1f36722dc44433c1d44865a9e54614483204d9ae
tesyaeriana <2141720024@student.polinema.ac.id>
2023-10-12 09:53:04 +0700

    final_praktikum

--- Layout dan Navigasi/layout_dan_navigasi/README.md
+++ Layout dan Navigasi/layout_dan_navigasi/README.md
@@ -1,16 +1,202 @@
-# layout_dan_navigasi
+# Pertemuan 7 Layout dan Navigasi
 
-A new Flutter project.
+Tesya Eriana/2141720024
 
-## Getting Started
+## Praktikum 1 Membangun Layout di Flutter
 
-This project is a starting point for a Flutter application.
+### Langkah 2
+```
+import 'package:flutter/material.dart';
 
-A few resources to get you started if this is your first Flutter project:
+void main() => runApp(const MyApp());
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout: Nama dan NIM Anda',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
+```
+Output :
+![Screnshoot hello_world](image/1.png)
+### Langkah 4
+```
+Widget titleSection = Container(
+    padding: const EdgeInsets.all(32.0),
+    child: Row(
+      children: [
+        Expanded(
+          /* soal 1*/
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // Jawaban Soal 1
+            children: [
+              /* soal 2*/
+              Container(
+                padding: const EdgeInsets.only(bottom: 8.0), // Jawaban Soal 2
+                child: const Text(
+                  'Wisata Gunung di Batu',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                'Batu, Malang, Indonesia',
+                style: TextStyle(
+                  color: Colors.grey, // Menggunakan Wrana Abu-Abu
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(32.0), // Jawaban Soal 3
+          child: Row(
+            children: [
+              /* soal 3*/
+              Icon(
+                Icons.star, // Untuk Icon Bintang
+                color: Colors.red, // Menggunakan warna merah
+              ),
+              const Text('41'),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+```
+Output :
+![Screnshoot hello_world](image/2.png)
+## Praktikum 2 Implementasi button row
+
+### Langkah 1: Buat method Column _buildButtonColumn
+```
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+```
+### Langkah 2: Buat widget buttonSection
+```
+Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: [
+    _buildButtonColumn(color, Icons.call, 'CALL'),
+    _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+    _buildButtonColumn(color, Icons.share, 'SHARE'),
+  ],
+```
+
+### Langkah 3: Tambah button section ke body
+```
+body: Column(
+           children: [
+             titleSection,
+              buttonSection,// Digunakan untuk menampilkan section yang telat dibuat
+           ],
+```
+
+Hasil Praktikum :
+![Screnshoot hello_world](image/3.png)
+
+## Praktikum 3: Implementasi text section
+### Langkah 1: Buat widget textSection
+```
+ Widget textSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: const Text(
+      'Carilah teks di internet yang sesuai '
+      'dengan foto atau tempat wisata yang ingin '
+      'Anda tampilkan. '
+      'Tambahkan nama dan NIM Anda sebagai '
+      'identitas hasil pekerjaan Anda. '
+      'Selamat mengerjakan 🙂.',
+      softWrap: true,
+    ),
+  );
+```
+### Langkah 2: Tambahkan variabel text section ke body
+```
+ body: Column(
+          children: [
+            titleSection,
+            buttonSection,
+            textSection, // Digunakan untuk menampilkan section yang telat dibuat
+          ],
+```
+
+Hasil Praktikum
+![Screnshoot hello_world](image/4.png)
+
+
+## Praktikum 4: Implementasi image section
+
+### Langkah 1: Siapkan aset gambar
+```
+flutter:
+  uses-material-design: true
+  assets:
+  - images/lake.jpg
+```
+
+### Langkah 2: Tambahkan gambar ke body
+```
+ children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+```
+
+Output :
+![Screnshoot hello_world](image/5.png)
+
+### Langkah 3: Terakhir, ubah menjadi ListView
+```
+body: ListView(
+          children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection, // Digunakan untuk menampilkan section yang telat dibuat
+          ],
+        ),
+```
--- Layout dan Navigasi/layout_dan_navigasi/lib/main.dart
+++ Layout dan Navigasi/layout_dan_navigasi/lib/main.dart
@@ -1,125 +1,117 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  //const MyApp({super.key});
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(32.0),
+    child: Row(
+      children: [
+        Expanded(
+          /* soal 1*/
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // Jawaban Soal 1
+            children: [
+              /* soal 2*/
+              Container(
+                padding: const EdgeInsets.only(bottom: 8.0), // Jawaban Soal 2
+                child: const Text(
+                  'Wisata Gunung di Batu',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                'Batu, Malang, Indonesia',
+                style: TextStyle(
+                  color: Colors.grey, // Menggunakan Wrana Abu-Abu
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(32.0), // Jawaban Soal 3
+          child: Row(
+            children: [
+              /* soal 3*/
+              Icon(
+                Icons.star, // Untuk Icon Bintang
+                color: Colors.red, // Menggunakan warna merah
+              ),
+              const Text('41'),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+  Widget textSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: const Text(
+      'Carilah teks di internet yang sesuai '
+      'dengan foto atau tempat wisata yang ingin '
+      'Anda tampilkan. '
+      'Tambahkan nama dan NIM Anda sebagai '
+      'identitas hasil pekerjaan Anda. '
+      'Selamat mengerjakan 🙂.',
+      softWrap: true,
+    ),
+  );
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      title: 'Flutter layout: Tesya Eriana/2141720024',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My App'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            titleSection,
+            buttonSection,
+            textSection, // Digunakan untuk menampilkan section yang telat dibuat
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
